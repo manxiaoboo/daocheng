4d651a95bfbb6267ee683b8d11837a97e90d37c8
xbman <manxiaobooo@126.com>
2017-12-24 23:33:45 +0800

    add cors

--- server/app.js
+++ server/app.js
@@ -13,6 +13,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1');
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+    });
+
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
