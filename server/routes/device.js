aa0554914411d31d50d2786faf7a9e93218f0f57
xbman <manxiaobooo@126.com>
2018-01-06 22:41:00 +0800

    update

--- server/routes/device.js
+++ server/routes/device.js
@@ -2,26 +2,52 @@ const express = require('express');
 const router = express.Router();
 const Device = require('../models/device');
 const UUID = require('uuid');
-const { isAuthenticated } = require('../auth/auth.service');
+const {
+    isAuthenticated
+} = require('../auth/auth.service');
 
 /**
  * 获取device列表
  */
-router.get('/', isAuthenticated(), async (req, res, next) => {
+router.get('/', isAuthenticated(), async(req, res, next) => {
     let devices = await Device.findAll();
+    res.json(devices);
+});
+
+/**
+ * 根据id获取device
+ */
+router.get('/getDeviceById', isAuthenticated(), async(req, res, next) => {
+    let device = await Device.findOne({
+        where:{
+            id:req.query.deviceId
+        }
+    });
+    res.json(device);
+});
+
+/**
+ * 获取对应状态的device列表
+ */
+router.post('/getDevicesByIsUse', isAuthenticated(), async(req, res, next) => {
+    let devices = await Device.findAll({
+        where: {
+            isUse: req.body.isUse
+        }
+    });
     res.json(devices);
 });
 
 /**
  * 创建device
  */
-router.post('/create', isAuthenticated(), async (req, res, next) => {
-    try{
+router.post('/create', isAuthenticated(), async(req, res, next) => {
+    try {
         let device = req.body;
         device.id = UUID.v1();
         let newDevice = await Device.create(device);
         res.json(newDevice);
-    }catch(e){
+    } catch (e) {
         res.status(500).json(e)
     }
 });
@@ -29,12 +55,12 @@ router.post('/create', isAuthenticated(), async (req, res, next) => {
 /**
  * 修改device
  */
-router.post('/edit', isAuthenticated(), async (req, res, next) => {
-    Device.update(req.body,{
-        where:{
-            id:req.body.id
+router.post('/edit', isAuthenticated(), async(req, res, next) => {
+    Device.update(req.body, {
+        where: {
+            id: req.body.id
         }
-    }).then((result)=>{
+    }).then((result) => {
         res.json(result);
     });
 });
@@ -42,7 +68,7 @@ router.post('/edit', isAuthenticated(), async (req, res, next) => {
 /**
  * 删除device
  */
-router.post('/delete', isAuthenticated(), async (req, res, next) => {
+router.post('/delete', isAuthenticated(), async(req, res, next) => {
     await Device.destroy({
         where: {
             id: req.body.id
